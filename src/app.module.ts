d2948070b39121daaa4fc8162468b1400cbf97cb
Oleh Shubin <shubinoleh@gmail.com>
2021-10-20 16:55:05 +0300

    add db connection, typeorm migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,7 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import conf from './config/env';
+import { Module, Global } from '@nestjs/common';
 
-const { pg } = conf;
-console.log(pg);
+@Global()
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      ...pg,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
 })
 export class AppModule {}
